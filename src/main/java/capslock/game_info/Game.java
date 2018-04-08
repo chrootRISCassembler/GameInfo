b1ea6762308f099f0903a3ffb4ecb720031c9efd
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-04-09 07:07:55 +0900

    new method query

--- src/main/java/capslock/game_info/Game.java
+++ src/main/java/capslock/game_info/Game.java
@@ -15,13 +15,12 @@
 
 package capslock.game_info;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.file.Path;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public abstract class Game {
     UUID uuid;
@@ -91,5 +90,90 @@ public abstract class Game {
         if (gameID > 0) json.put("gameID", gameID);
 
         return json;
+    }
+
+    /**
+     * {@link Field}に対応した値を取得する.
+     * <p>
+     *     このメソッドは{@link Object}型で値を返すため,濫用するべきでない.
+     *     キャストが必要なとき,このメソッドはたいてい正しく使われていない.
+     * </p>
+     * @param field 値を取得する{@link Field}
+     * @return 値が設定されているとき その値. 値が設定されていないとき {@code null}.
+     */
+    final Object getFieldValue(Field field){
+        //Enumでswitchするのは設計ミスかもしれない
+        switch (field){
+            case UUID:
+                return uuid;
+            case EXE:
+                return exe;
+            case NAME:
+                return name;
+            case DESC:
+                return desc;
+            case PANEL:
+                return panel;
+            case MOVIE_LIST:
+                return movieList.isEmpty() ? null : movieList ;
+            case IMAGE_LIST:
+                return imageList.isEmpty() ? null : imageList;
+            case GAME_ID:
+                return gameID > 0 ? gameID : null;
+            case LAST_MOD:
+                return lastMod;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    public final String query(EnumSet<Field> fieldSet){
+        final var builder = new StringBuilder("{");
+
+        boolean isFirst = true;
+        for (final var field : fieldSet){
+            final var fieldValue = getFieldValue(field);
+            if(fieldValue == null)continue;
+
+            if(isFirst){
+                isFirst = false;
+            }else {
+                builder.append(", ");
+            }
+
+            builder.append(field);
+            builder.append(" : ");
+            builder.append(fieldValue.toString());
+        }
+
+        builder.append('}');
+        return builder.toString();
+    }
+
+    public final String query(String jsonQuery) throws IllegalArgumentException {
+        final JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(jsonQuery);
+        }catch (JSONException ex){
+            throw new IllegalArgumentException(ex);
+        }
+
+        final EnumSet<Field> fieldEnumSet = EnumSet.noneOf(Field.class);
+        boolean isUUIDOutExplicitly = false;
+
+        for(final var field : Field.values()){
+            try {
+                if (jsonObject.getBoolean(field.toString())) {
+                    fieldEnumSet.add(field);
+                } else {
+                    if (field == Field.UUID) isUUIDOutExplicitly = true;
+                }
+            }catch (JSONException ex){
+                throw  new IllegalArgumentException(ex);
+            }
+        }
+
+        if(!isUUIDOutExplicitly)fieldEnumSet.add(Field.UUID);
+        return query(fieldEnumSet);
     }
 }
